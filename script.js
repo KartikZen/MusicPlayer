fabf121142c6b490948a9d38e759a1853ad750ad
Kartik Yadav <ky340425@gmail.com>
2025-10-06 23:27:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,400 @@
+        // Playlists by singer
+        const playlists = {
+            arijit: [
+                {
+                    name: "Kesariya",
+                    artist: "Arijit Singh",
+                    src: "audio/Kesariya.mp3",
+                    img: "img/kesariya.jpg"
+                },
+                {
+                    name: "Apna Bana Le",
+                    artist: "Arijit Singh",
+                    src: "audio/Apna Bana Le Bhediya.mp3",
+                    img: "img/Apna-Bana-Le-Bhediya-500-500.jpg"
+                },
+                {
+                    name: "Tum Hi Ho",
+                    artist: "Arijit Singh",
+                    src: "audio/Tum Hi Ho Aashiqui 2.mp3",
+                    img: "img/Tum-Hi-Ho-Aashiqui-2-500-500.jpg"
+                },
+                {
+                    name: "Shayad",
+                    artist: "Arijit Singh",
+                    src: "audio/Shayad Love Aaj Kal.mp3",
+                    img: "img/Shayad-Love-Aaj-Kal-500-500.jpg"
+                },
+                {
+                    name: "Channa Mereya",
+                    artist: "Arijit Singh",
+                    src: "audio/Channa Mereya.mp3",
+                    img: "img/Channa-Mereya-Ae-Dil-Hai-Mushkil-500-500.jpg"
+                },
+                {
+                    name: "Tera Yaar Hoon Main",
+                    artist: "Arijit Singh",
+                    src: "audio/Tera Yaar Hoon Main.mp3",
+                    img: "img/Tera-Yaar-Hoon-Main-Sonu-Ke-Titu-Ki-Sweety-500-500.jpg"
+                },
+                {
+                    name: "Raabta",
+                    artist: "Arijit Singh",
+                    src: "audio/Raabta.mp3",
+                    img: "img/raabta.jpg"
+                },
+                {
+                    name: "Phir Le Aya Dil",
+                    artist: "Arijit Singh",
+                    src: "audio/Phir Le Aya Dil.mp3",
+                    img: "img/Phir-Le-Aya-Dil.jpg"
+                },
+                {
+                    name: "Agar Tum Saath Ho",
+                    artist: "Alka Yagnik, Arijit Singh",
+                    src: "audio/Agar Tum Saath Ho.mp3",
+                    img: "img/Agar-Tum-Saath-Ho.jpg"
+                },
+                {
+                    name: "Hawayein",
+                    artist: "Arijit Singh",
+                    src: "audio/Hawayein.mp3",
+                    img: "img/Hawayein.jpg"
+                },
+                {
+                    name: "Saiyaara",
+                    artist: "Arijit Singh",
+                    src: "audio/Saiyaara.mp3",
+                    img: "img/saiyaara.jpg"
+                },
+                {
+                    name: "Dhun",
+                    artist: "Arijit Singh",
+                    src: "audio/Dhun.mp3",
+                    img: "img/Arjit2.webp"
+                },
+                {
+                    name: "Bulleya",
+                    artist: "Arijit Singh",
+                    src: "audio/Bulleya.mp3",
+                    img: "img/Ae-Dil-Hai-Mushkil.jpg"
+                },
+                {
+                    name: "Humsafar",
+                    artist: "Arijit Singh",
+                    src: "audio/Humsafar.mp3",
+                    img: "img/humsafar.jpg"
+                },
+                {
+                    name: "Ik Vaari Aa",
+                    artist: "Arijit Singh",
+                    src: "audio/Ik Vaari Aa.mp3",
+                    img: "img/raabta.jpg"
+                },
+                {
+                    name: "Lambiyaan Si Judaiyaan",
+                    artist: "Arijit Singh",
+                    src: "audio/Lambiyaan Si Judaiyaan.mp3",
+                    img: "img/raabta.jpg"
+                },
+                {
+                    name: "Janam Janam",
+                    artist: "Arijit Singh",
+                    src: "audio/Janam Janam.mp3",
+                    img: "img/Janam-Janam-Dilwale.jpg"
+                }
+
+            ],
+            jubin: [
+                {
+                    name: "Lut Gaye",
+                    artist: "Jubin Nautiyal",
+                    src: "audio/Jubin/Lut Gaye.mp3",
+                    img: "img/lut-gaye.jpg"
+                },
+                {
+                    name: "Tujhe Kitna Chahein Aur",
+                    artist: "Jubin Nautiyal",
+                    src: "audio/Jubin/Tujhe Kitna Chahein Aur.mp3",
+                    img: "img/tujhe-kitna-chahein-aur.jpg"
+                },
+                {
+                    name: "Raataan Lambiyan",
+                    artist: "Jubin Nautiyal",
+                    src: "audio/Jubin/Raataan Lambiyan.mp3",
+                    img: "img/raataan-lambiyan.jpg"
+                }
+            ],
+            vishal: [
+                {
+                    name: "Kaise Hua",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Kaise Hua.mp3",
+                    img: "img/Kaise-Hua.jpg"
+                },
+                {
+                    name: "Pehle Bhi Main",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Pehle Bhi Main.mp3",
+                    img: "img/jina.webp"
+                },
+                {
+                    name: "Aaj Bhi",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Aaj Bhi.mp3",
+                    img: "img/aaj-bhi.jpg"
+                },
+                {
+                    name: "Manjha",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Manjha.mp3",
+                    img: "img/manjha.webp"
+                },
+                {
+                    name: "Jeena Haraam",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Jeena Haraam.mp3",
+                    img: "img/jina.webp"
+                },
+                {
+                    name: "Deewaniyat",
+                    artist: "Vishal Mishra",
+                    src: "audio/Vishal/Deewaniyat.mp3",
+                    img: "img/deewaniyat.webp"
+                }
+
+            ],
+            Honey: [
+                {
+                    name: "Brown Rang",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/Brown Rang.mp3",
+                    img: "img/brown-rang.webp"
+                },
+                {
+                    name: "Dheere Dheere",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/Dheere Dheere.mp3",
+                    img: "img/lungi.webp"
+                },
+                {
+                    name: "High Heels",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/High Heels.mp3",
+                    img: "img/lungi.webp"
+                },
+                {
+                    name: "Lungi Dance",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/Lungi Dance.mp3",
+                    img: "img/lungi.webp"
+                },
+                {
+                    name: "Party All Night",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/Party All Night.mp3",
+                    img: "img/lungi.webp"
+                },
+                {
+                    name: "Blue Eyes",
+                    artist: "Yo Yo Honey Singh",
+                    src: "audio/Honey/Blue Eyes.mp3",
+                    img: "img/lungi.webp"
+                }
+            ]
+
+        };
+
+        // Default playlist is Arijit
+        let currentPlaylist = playlists.arijit;
+        let currentSong = 0;
+
+        // DOM Elements
+        const audio = document.getElementById('audio');
+        const playBtn = document.getElementById('play-btn').querySelector('i');
+        const progressBar = document.getElementById('progress-bar');
+        const prevBtn = document.getElementById('prev-btn');
+        const nextBtn = document.getElementById('next-btn');
+        const songName = document.getElementById('song-name');
+        const songArtist = document.getElementById('song-artist');
+        const songImg = document.getElementById('song-img');
+        const playlistBtn = document.getElementById('playlist-btn');
+        const playlistModal = document.getElementById('playlist-modal');
+        const playlistList = document.getElementById('playlist-list');
+        const closePlaylist = document.getElementById('close-playlist');
+        const backBtn = document.getElementById('back-btn');
+        const menuBtn = document.getElementById('menu-btn');
+        const categoryBtns = document.querySelectorAll('.playlist-category-btn');
+
+        // Time & Duration Elements
+        const currentTimeEl = document.getElementById('current-time');
+        const durationEl = document.getElementById('duration');
+        const volumeBar = document.getElementById('volume-bar');
+
+        // Format seconds to mm:ss
+        function formatTime(sec) {
+            sec = Math.floor(sec);
+            const min = Math.floor(sec / 60);
+            const s = sec % 60;
+            return `${min}:${s < 10 ? '0' : ''}${s}`;
+        }
+
+        // Load Song
+        function loadSong(index) {
+            const song = currentPlaylist[index];
+            audio.src = song.src;
+            songName.textContent = song.name;
+            songArtist.textContent = song.artist;
+            songImg.src = song.img;
+            currentSong = index;
+            progressBar.value = 0;
+            currentTimeEl.textContent = "0:00";
+            durationEl.textContent = "0:00";
+            // Wait for metadata to update duration
+            audio.addEventListener('loadedmetadata', () => {
+                durationEl.textContent = formatTime(audio.duration);
+            }, { once: true });
+        }
+
+        // Play/Pause
+        function togglePlay() {
+            if (audio.paused) {
+                audio.play();
+                playBtn.classList.remove('bi-play-fill');
+                playBtn.classList.add('bi-pause-fill');
+            } else {
+                audio.pause();
+                playBtn.classList.remove('bi-pause-fill');
+                playBtn.classList.add('bi-play-fill');
+            }
+        }
+
+        playBtn.parentElement.addEventListener('click', togglePlay);
+
+        // Update Progress Bar
+        audio.addEventListener('timeupdate', () => {
+            currentTimeEl.textContent = formatTime(audio.currentTime);
+            durationEl.textContent = formatTime(audio.duration);
+            if (audio.duration) {
+                const progress = (audio.currentTime / audio.duration) * 100;
+                progressBar.value = progress;
+            }
+        });
+
+        // Seek song
+        progressBar.addEventListener('input', () => {
+            const seekTime = (progressBar.value / 100) * audio.duration;
+            audio.currentTime = seekTime;
+        });
+
+        // Volume control
+        volumeBar.addEventListener('input', () => {
+            audio.volume = volumeBar.value;
+        });
+
+        // Next Song
+        function nextSong() {
+            loadSong((currentSong + 1) % currentPlaylist.length);
+            audio.play();
+            playBtn.classList.remove('bi-play-fill');
+            playBtn.classList.add('bi-pause-fill');
+            songImg.style.animationPlayState = 'running';
+
+        }
+
+        // Previous Song
+        function prevSong() {
+            if (audio.currentTime > 3) {
+                audio.currentTime = 0;
+            } else {
+                loadSong((currentSong - 1 + currentPlaylist.length) % currentPlaylist.length);
+                audio.play();
+                playBtn.classList.remove('bi-play-fill');
+                playBtn.classList.add('bi-pause-fill');
+                songImg.style.animationPlayState = 'running';
+
+            }
+        }
+
+        nextBtn.addEventListener('click', nextSong);
+        prevBtn.addEventListener('click', prevSong);
+
+        // Auto play next song when current ends
+        audio.addEventListener('ended', () => {
+            nextSong();  // play next automatically
+        });
+
+
+        // Playlist Modal
+        playlistBtn.addEventListener('click', () => {
+            playlistModal.style.display = 'flex';
+        });
+        closePlaylist.addEventListener('click', () => {
+            playlistModal.style.display = 'none';
+        });
+        playlistModal.addEventListener('click', (e) => {
+            if (e.target === playlistModal) playlistModal.style.display = 'none';
+        });
+
+        // Render Playlist
+        function renderPlaylist() {
+            playlistList.innerHTML = '';
+            currentPlaylist.forEach((song, idx) => {
+                const li = document.createElement('li');
+                li.innerHTML = `<img src="${song.img}" alt="" style="width:32px;height:32px;border-radius:6px;object-fit:cover;"> 
+                                <span>${song.name}</span> - <small>${song.artist}</small>`;
+                li.addEventListener('click', () => {
+                    loadSong(idx);
+                    audio.play();
+                    playBtn.classList.remove('bi-play-fill');
+                    playBtn.classList.add('bi-pause-fill');
+                    playlistModal.style.display = 'none';
+                });
+                playlistList.appendChild(li);
+            });
+        }
+
+        // Playlist Category Dropdown
+        const singerSelect = document.getElementById('singer-select');
+
+        singerSelect.addEventListener('change', function () {
+            const cat = this.value;
+            currentPlaylist = playlists[cat];
+            currentSong = 0;
+            loadSong(0);
+            renderPlaylist();
+        });
+
+        // Back/Menu Buttons (customize as needed)
+        backBtn.addEventListener('click', () => {
+            alert('Back button clicked! (You can add navigation here)');
+        });
+        menuBtn.addEventListener('click', () => {
+            alert('Menu button clicked! (You can add more menu features here)');
+        });
+
+        // Initial Load (set dropdown value)
+        singerSelect.value = "arijit";
+        loadSong(currentSong);
+        renderPlaylist();
+        // Start with paused rotation
+        function togglePlay() {
+            if (audio.paused) {
+                audio.play();
+                playBtn.classList.remove('bi-play-fill');
+                playBtn.classList.add('bi-pause-fill');
+                songImg.style.animationPlayState = 'running';  // start rotation
+            } else {
+                audio.pause();
+                playBtn.classList.remove('bi-pause-fill');
+                playBtn.classList.add('bi-play-fill');
+                songImg.style.animationPlayState = 'paused';   // stop rotation
+            }
+        }
+
+        // Also stop rotation when song ends
+        audio.addEventListener('ended', () => {
+            playBtn.classList.remove('bi-pause-fill');
+            playBtn.classList.add('bi-play-fill');
+            songImg.style.animationPlayState = 'paused';
+        });
